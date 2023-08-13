bbd3be0dad84ee3e88f66fd23dc3d61959201a69
zane grosse <zanekg123@gmail.com>
2023-08-12 23:55:23 -0500

    test usestate

--- app/components/nav.js
+++ app/components/nav.js
@@ -1,4 +1,7 @@
+"use client"
+import { useState } from "react";
 export default function Nav() {
+    const [first, setfirst] = useState(false)
     return <>
         <div className="flex border=text border-b-[1px]">
             <div className="cell border=text px-6 py-4 h-full">
@@ -6,7 +9,7 @@ export default function Nav() {
             </div>
             <div className="spacer flex-grow"></div>
              <div className="cell border=text">
-                <h1 className="font-bold text-5xl px-5 py-4 h-full">ZANE</h1>
+                <button className="font-bold text-5xl px-5 py-4 h-full">ABOUT</button>
             </div>           
         </div>
     </>;
