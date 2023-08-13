9168128a2d72917a93b4f4c7e9fe3537afda8bc9
zane grosse <zanekg123@gmail.com>
2023-08-13 17:59:21 -0500

    add to nav

--- app/components/nav.js
+++ app/components/nav.js
@@ -1,16 +1,45 @@
-"use client"
-import { useState } from "react";
+"use client";
+import { useEffect, useState } from "react";
 export default function Nav() {
-    const [first, setfirst] = useState(false)
-    return <>
-        <div className="flex border=text border-b-[1px]">
-            <div className="cell border=text px-6 py-4 h-full">
-                <h1 className="font-bold text-5xl">ZANE</h1>
+    const [date, setDate] = useState("Thursday, 1 January, 1970");
+    useEffect(() => {
+        const options = {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+        };
+        setDate(new Date().toLocaleDateString("en-US", options));
+    }, []);
+    return (
+        <>
+            <div className="">
+                <div className="flex">
+                    <div className="cell border=text px-6 py-4 h-full">
+                        <a
+                            href="/"
+                            className="font-semibold text-5xl hover:underline"
+                        >
+                            <span className="font-extralight">://</span>
+                            zaneg.net
+                        </a>
+                    </div>
+                    <div className="spacer flex-grow"></div>
+                    <div className="cell border=text">
+                        <button className="font-bold text-5xl px-5 py-4 h-full ">
+                            about
+                        </button>
+                    </div>
+                </div>
+                <div className="border=text border-b-[1px] border-t-[1px]  px-4 py-2">
+                    <p className="font-serif">
+                        {date}.
+                        <span className="pl-3 ml-3 border-l-[1px] border=text font-serif">
+                            {"last updated 4 days ago."}
+                        </span>
+                    </p>
+                </div>
             </div>
-            <div className="spacer flex-grow"></div>
-             <div className="cell border=text">
-                <button className="font-bold text-5xl px-5 py-4 h-full">ABOUT</button>
-            </div>           
-        </div>
-    </>;
+        </>
+    );
 }
