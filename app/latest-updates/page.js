839e055d714d21d7e4dd836f0ff70c01ac11f76b
zane grosse <zanekg123@gmail.com>
2023-08-19 21:06:14 -0500

    try to fix missing svg on /latest-updates.

--- app/latest-updates/page.js
+++ app/latest-updates/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import Nav from "../components/nav";
-import AiOutlineDown from "../components/AiOutlineDown.js";
+import AiOutlineDown from "../components/AiOutlineDown";
 
 async function getData() {
     const res = await fetch(
