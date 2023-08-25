b07cb82327c5f5dcf0b008aec4ca13699a4aab49
zane grosse <zanekg123@gmail.com>
2023-08-24 23:26:36 -0500

    there we go. nav flashiing fixed. knd of messy though via only adding transition after 100ms.

--- app/components/nav.js
+++ app/components/nav.js
@@ -7,7 +7,7 @@ export default function Nav() {
 
     const [latest, setLatest] = useState({ ago: "", date: "", message: "" });
 
-    const [isLoaded, setIsLoaded] = useState(false)
+    const [isLoaded, setIsLoaded] = useState(false);
 
     const [aboutHeight, setAboutHeight] = useState(0);
 
@@ -35,25 +35,29 @@ export default function Nav() {
         let latestChange;
 
         latestChanges().then((result) => {
-      setLatest(result);
+            setLatest(result);
         });
 
-        setIsLoaded(true);
         setAboutHeight(ref.current.clientHeight);
-        ref.current.classList.remove("absolute")
+        ref.current.classList.remove("absolute");
+        setTimeout(function () {
+            setIsLoaded(true);
+        }, 100);
     }, []);
     return (
         <>
             <div
-            ref={ref2}
+                ref={ref2}
                 style={{
                     marginTop: `-${!aboutToggled ? aboutHeight + 1 : 0}px`,
-                    transition: `${isLoaded? ".3s" : "none"}`,
-
+                    transition: `${isLoaded ? ".3s" : "none"}`,
                 }}
-        className="bg-alternate z-[5] "
+                className="bg-alternate z-[5] "
             >
-                <div className="border=text border-b-[1px] absolute z-[-5]" ref={ref}>
+                <div
+                    className="border=text border-b-[1px] absolute z-[-5]"
+                    ref={ref}
+                >
                     About content.
                 </div>
                 <div className="flex">
@@ -76,18 +80,17 @@ export default function Nav() {
                     </div>
                 </div>
             </div>
-                <div className="border=text border-b-[1px] border-t-[1px] sticky top-0 px-4 py-2 bg-alternate glass">
-                    <p className="text-sm font-semibold capitalized">
-                        {date}.
-                        <a
-                            href="/latest-changes"
-                            className="hover:underline font-medium pl-3 ml-3 border-l-[1px] border=text"
-                        >
-                            Last updated {latest.ago}
-                        </a>
-                    </p>
-                </div>
-
+            <div className="border=text border-b-[1px] border-t-[1px] sticky top-0 px-4 py-2 bg-alternate glass">
+                <p className="text-sm font-semibold capitalized">
+                    {date}.
+                    <a
+                        href="/latest-changes"
+                        className="hover:underline font-medium pl-3 ml-3 border-l-[1px] border=text"
+                    >
+                        Last updated {latest.ago}
+                    </a>
+                </p>
+            </div>
         </>
     );
 }
